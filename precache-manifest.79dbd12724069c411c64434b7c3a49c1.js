57e1e9c3b2ddc994100981edebd34d0020f7ead0
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-04 11:22:19 +0100

    Updates

---
+++ precache-manifest.79dbd12724069c411c64434b7c3a49c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9765b27f8975c69a36a9674fe30e576e",
+    "url": "/navno-navigering-poc/index.html"
+  },
+  {
+    "revision": "961108e26e1d3aac922e",
+    "url": "/navno-navigering-poc/static/css/2.9371a1a6.chunk.css"
+  },
+  {
+    "revision": "c2873b5be09be08b076a",
+    "url": "/navno-navigering-poc/static/css/main.2e7e6113.chunk.css"
+  },
+  {
+    "revision": "961108e26e1d3aac922e",
+    "url": "/navno-navigering-poc/static/js/2.463f4129.chunk.js"
+  },
+  {
+    "revision": "334968b1725ea933dde239169d6a9b50",
+    "url": "/navno-navigering-poc/static/js/2.463f4129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2873b5be09be08b076a",
+    "url": "/navno-navigering-poc/static/js/main.954cd065.chunk.js"
+  },
+  {
+    "revision": "01ea56d76cccb8d88bf6",
+    "url": "/navno-navigering-poc/static/js/runtime-main.46cddf28.js"
+  }
+]);
