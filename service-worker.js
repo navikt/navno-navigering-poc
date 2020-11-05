213dce19542b556e29a07090d00de915b79275be
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-05 11:15:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/navno-navigering-poc/precache-manifest.59044c6432a8356fa5d0fa75695da4c9.js"
+  "/navno-navigering-poc/precache-manifest.5247531817e3cd807a580e559e4ed68c.js"
 );
 
 self.addEventListener('message', (event) => {
