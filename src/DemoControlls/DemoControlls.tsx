a154046aeb2893fb9d26f90b2741d7d6e7e9bdba
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-06 13:48:30 +0100

    gjør om til checkboxer i demo-controlls

--- src/DemoControlls/DemoControlls.tsx
+++ src/DemoControlls/DemoControlls.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { theme } from "../theme";
 import { useRef, useState } from "react";
 import { useClickAway } from "react-use";
+import { guid } from "nav-frontend-js-utils";
 
 const Wrapper = styled.div`
   position: absolute;
@@ -19,7 +20,7 @@ const PopDown = styled.div`
   position: absolute;
   padding: 1rem;
   background-color: white;
-  min-width: 15rem;
+  min-width: 18rem;
   border: 0.2rem ${theme.colors.navLimeGronnLighten80} solid;
   filter: drop-shadow(0.2rem 0.2rem 3rem black);
 `;
@@ -30,6 +31,7 @@ const MenuButton = styled.button`
   border: 0.2rem solid ${theme.colors.navLimeGronn};
   padding: 0.5rem;
   opacity: 0.6;
+  z-index: 10;
 `;
 
 const Grid = styled.div`
@@ -38,12 +40,36 @@ const Grid = styled.div`
   grid-gap: 1rem;
 `;
 
-const Knapp = styled.button`
-  background-color: transparent;
-  padding: 0.7rem;
-  border: 0.2rem solid ${theme.colors.navLimeGronnLighten80};
-  border-radius: 0.4rem;
+const CheckBoxStyle = styled.div`
+  font-size: 1.2rem;
+  display: flex;
+  align-items: center;
+  input {
+    width: 1.3rem;
+    height: 1.3rem;
+    margin-right: 0.5rem;
+  }
 `;
+
+function CheckBox(props: {
+  label: string;
+  checked: boolean;
+  onClick: () => void;
+}) {
+  const id = useRef(guid()).current;
+
+  return (
+    <CheckBoxStyle>
+      <input
+        id={id}
+        type="checkbox"
+        checked={props.checked}
+        onClick={props.onClick}
+      />
+      <label htmlFor={id}>{props.label}</label>
+    </CheckBoxStyle>
+  );
+}
 
 function DemoControlls() {
   const [context, dispatch] = useDemoContext();
@@ -52,40 +78,41 @@ function DemoControlls() {
   useClickAway(ref, () => setShow(false));
 
   return (
-    <Wrapper>
+    <Wrapper ref={ref}>
+      <MenuButton onClick={() => setShow(!show)}>DC</MenuButton>
       {show && (
-        <PopDown ref={ref}>
+        <PopDown>
           <h2>Demo-controlls:</h2>
           <Grid>
-            <Knapp
+            <CheckBox
               onClick={() => dispatch(context.visMeny ? "ingenMeny" : "meny")}
-            >
-              {context.visMeny ? "Skjul" : "Vis"} meny
-            </Knapp>
-            <Knapp
+              label="Vis meny"
+              checked={context.visMeny}
+            />
+            <CheckBox
               onClick={() =>
                 dispatch(context.visIkoner ? "skjulIkoner" : "visIkoner")
               }
-            >
-              {context.visIkoner ? "Skjul" : "Vis"} ikoner
-            </Knapp>
-            <Knapp
-              onClick={() =>
-                dispatch(context.langMeny ? "kortMeny" : "langMeny")
-              }
-            >
-              {context.langMeny ? "Skjul" : "Vis"} underpunkter i meny
-            </Knapp>
-            <Knapp
+              label="Vis ikoner"
+              checked={context.visIkoner}
+            />
+            <CheckBox
+              onClick={() => {
+                dispatch(context.langMeny ? "kortMeny" : "langMeny");
+              }}
+              label="Vis underpunkter i meny"
+              checked={context.langMeny}
+            />
+            <CheckBox
               onClick={() =>
                 dispatch(
                   context.visBrødsmuler ? "skjulBrødsmuler" : "visBrødsmuler"
                 )
               }
-            >
-              {context.visBrødsmuler ? "Skjul" : "Vis"} brødsmuler
-            </Knapp>
-            <Knapp
+              label="Vis brødsmuler"
+              checked={context.visBrødsmuler}
+            />
+            <CheckBox
               onClick={() =>
                 dispatch(
                   context.undersiderPaForside
@@ -93,14 +120,12 @@ function DemoControlls() {
                     : "visUndersiderPaForside"
                 )
               }
-            >
-              {context.undersiderPaForside ? "Skjul" : "Vis"} undersider på
-              forside
-            </Knapp>
+              label="Vis undersider på forside"
+              checked={context.undersiderPaForside}
+            />
           </Grid>
         </PopDown>
       )}
-      <MenuButton onClick={() => setShow(!show)}>DC</MenuButton>
     </Wrapper>
   );
 }
