738140f60cea0737fd744e3ebe2ce1660da4582c
winsvold <danielwinsvold@gmail.com>
2020-11-13 14:35:50 +0000

    Deploying to gh-pages from  @ 3f198c9f93b8e3bc3c31979fa94162d0e831350e 🚀

--- precache-manifest.0570840ecdcb42fa80ec00ed89f203ef.js
+++ precache-manifest.0570840ecdcb42fa80ec00ed89f203ef.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c575cb94e7961b3cb8ea40f826b7b693",
+    "revision": "9ef335eea78dc76d994142f5b34033de",
     "url": "/navno-navigering-poc/index.html"
   },
   {
-    "revision": "4b4fb34debd754b25290",
+    "revision": "c6ebb95bfe4a182d3158",
     "url": "/navno-navigering-poc/static/css/2.6f7985af.chunk.css"
   },
   {
-    "revision": "7b8a6c26781087c2dd8e",
+    "revision": "d92a92881a53107ed639",
     "url": "/navno-navigering-poc/static/css/main.022cf9f3.chunk.css"
   },
   {
-    "revision": "4b4fb34debd754b25290",
-    "url": "/navno-navigering-poc/static/js/2.459d0489.chunk.js"
+    "revision": "c6ebb95bfe4a182d3158",
+    "url": "/navno-navigering-poc/static/js/2.3ea811fd.chunk.js"
   },
   {
     "revision": "334968b1725ea933dde239169d6a9b50",
-    "url": "/navno-navigering-poc/static/js/2.459d0489.chunk.js.LICENSE.txt"
+    "url": "/navno-navigering-poc/static/js/2.3ea811fd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b8a6c26781087c2dd8e",
-    "url": "/navno-navigering-poc/static/js/main.067c0687.chunk.js"
+    "revision": "d92a92881a53107ed639",
+    "url": "/navno-navigering-poc/static/js/main.f7805ef0.chunk.js"
   },
   {
     "revision": "01ea56d76cccb8d88bf6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/navno-navigering-poc/precache-manifest.afa635bcf65ca1376289c69d1884fcbc.js"
+  "/navno-navigering-poc/precache-manifest.0570840ecdcb42fa80ec00ed89f203ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/navno-navigering-poc/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
