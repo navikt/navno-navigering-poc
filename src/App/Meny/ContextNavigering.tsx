cf3dda855401ca656fee253b09a87a245af260f6
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-22 13:21:24 +0100

    strek under valgt context

--- src/App/Meny/ContextNavigering.tsx
+++ src/App/Meny/ContextNavigering.tsx
@@ -1,18 +1,18 @@
 import * as React from "react";
-import styled from "styled-components/macro";
+import styled, { css } from "styled-components/macro";
 import { theme } from "../../theme";
 import { useNavigasjon } from "../useNavigasjon";
 import { getOmrådeFraTittel } from "../../data/menuDataUtils";
 
 const Style = styled.div`
-  padding: 0.5rem;
+  padding: 0.5rem 1rem 0;
   border-bottom: 0.1rem solid ${theme.colors.navLysBla};
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 `;
 
-const Knapp = styled.button`
+const Knapp = styled.button<{ selected: boolean }>`
   background-color: transparent;
   border: none;
   font-weight: bold;
@@ -21,18 +21,40 @@ const Knapp = styled.button`
   &:hover {
     opacity: 0.8;
   }
+  border-bottom: 0.25rem solid transparent;
+  ${(props) =>
+    props.selected &&
+    css`
+      border-bottom-color: ${theme.colors.navBla};
+    `}
 `;
 
+const arbeidsgiverTittel = "Arbeidsgiver";
+const samarbeidspartnerTittel = "Samarbeidspartner";
+
 function ContextNavigering() {
-  const { navigerTil } = useNavigasjon();
+  const { navigerTil, område } = useNavigasjon();
 
   return (
     <Style>
-      <Knapp onClick={() => navigerTil()}>Person</Knapp>
-      <Knapp onClick={() => navigerTil(getOmrådeFraTittel("Arbeidsgiver"))}>
+      <Knapp
+        selected={
+          ![arbeidsgiverTittel, samarbeidspartnerTittel].includes(
+            område?.title || ""
+          )
+        }
+        onClick={() => navigerTil()}
+      >
+        Person
+      </Knapp>
+      <Knapp
+        selected={arbeidsgiverTittel === område?.title}
+        onClick={() => navigerTil(getOmrådeFraTittel("Arbeidsgiver"))}
+      >
         Arbeidsgiver
       </Knapp>
       <Knapp
+        selected={samarbeidspartnerTittel === område?.title}
         onClick={() => navigerTil(getOmrådeFraTittel("Samarbeidspartner"))}
       >
         Samarbeidspartner
