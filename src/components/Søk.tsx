5ce24d4b7de2bd49c0349020ab32cb005ba0bb5c
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-22 12:36:16 +0100

    varsel om at søk ikke funker hvis man prøver

--- src/components/Søk.tsx
+++ src/components/Søk.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import { IkonButtonStyle } from "./MenyButton";
 import SøkIkon from "../Ikoner/SøkIkon";
 import styled from "styled-components/macro";
+import { useRef, useState } from "react";
+import { useClickAway } from "react-use";
+import { theme } from "../theme";
 
 const Style = styled(IkonButtonStyle)`
+  position: relative;
   margin-left: 1rem;
   svg {
     height: 1.4rem;
@@ -12,11 +16,33 @@ const Style = styled(IkonButtonStyle)`
   }
 `;
 
+const FunkerikkeWarning = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 15rem;
+  padding: 1.5rem;
+  background-color: white;
+  border: solid 0.3rem ${theme.colors.redError};
+  border-radius: 0.3rem;
+  box-shadow: 0 0 4rem #888;
+`;
+
 function Søk(props: React.HTMLAttributes<HTMLButtonElement>) {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useClickAway(ref, () => setOpen(false));
+
   return (
-    <Style {...props}>
+    <Style {...props} ref={ref} onClick={() => setOpen(!open)}>
       <SøkIkon />
       <span>Søk</span>
+      {open && (
+        <FunkerikkeWarning>
+          Det er dessverre ikke mulig å bruke søk på denne testsiden
+        </FunkerikkeWarning>
+      )}
     </Style>
   );
 }
